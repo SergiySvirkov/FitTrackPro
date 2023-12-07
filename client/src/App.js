0235ec0d27a4abe14625ba5fe9c34b2b0cf9b619
SergiySvirkov <124861145+SergiySvirkov@users.noreply.github.com>
2023-12-07 13:19:19 +0200

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
+// Import other screens as needed
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        {/* Add other screens as needed */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
