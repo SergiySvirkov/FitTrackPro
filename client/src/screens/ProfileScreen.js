86e35fbd3284e62b0eabdfbf4e1a49365fa7dc3d
SergiySvirkov <124861145+SergiySvirkov@users.noreply.github.com>
2023-12-07 13:28:15 +0200

    Add files via upload

---
+++ client/src/screens/ProfileScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+const ProfileScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Profile Screen</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+    </View>
+  );
+};
+
+export default ProfileScreen;
+
